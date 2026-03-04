20d0237ffb207bb67d1664931658696f45e70f8d
JasonMayemba <jasonnsiamfumu1@gmail.com>
2026-03-04 12:42:58 +0100

    day 2.py

---
+++ Problem/day 2.py
@@ -0,0 +1,14 @@
+"""
+2. Add Two Numbers
+difficulté : medium
+
+
+"""
+
+class ListNode:
+  def __init__(self, val=0, newt=None):
+    self.val = val
+    self.next = next
+class Solution:
+  def addTwoNumbers(self, l1 : Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    
